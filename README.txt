de8b346da5a130f39366559cd6b5f8197ea5f611
BrandonMBurt154 <Brandonmburt154@gmail.com>
2021-03-16 22:29:27 -0400

    Complete rework

--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 ***WIP***
 
-A Simple command to locally display name and phone number in chat designed for FiveM
+A Simple command to locally display name and phone number in chat designed using essentialmode 
 
 Place in your resource folder and ensure PhoneNumberCommand in your server.cfg
--- client.lua
+++ client.lua
@@ -1,12 +1,32 @@
-wait = false
-
-RegisterCommand('p#', function(source, args) -- Registers command that will display phone number locally in chat
-    if wait == false then
-        TriggerServerEvent('DisplayPhone', args)
-        wait = true
-        Citizen.Wait(20000) -- Time out on the command that stops people from spamming it 
-        wait = false
-    elseif wait == true then
-        TriggerEvent('chatMessage', "", {255, 0, 0}, "Don't Spam Pwease >_<" ) -- Client side chat message displayed if player attempts to repeat the command during the time out
-	end
-end
+Citizen.CreateThread(function()
+    ESX.TriggerServerCallback('info', function(data)
+        local firstname = nil
+        local lastname = nil
+        local phone = nil
+            if data.firstname ~= nil then
+                firstname = data.firstname
+            else
+                firstname = 'Unknow'
+            end
+            if data.lastname ~= nil then
+                lastname = data.lastname
+            else
+                lastname = 'Unknow'
+            end                              
+            if data.phone ~= nil then        
+                phone = data.phone           
+            else                             
+                phone = 'Unknow'             
+            end                              
+    
+    RegisterNetEvent('chat:phonenum')
+    AddEventHandler('chat:phonenum', function(id, name, message)
+        local myId = PlayerId() 
+        local otherID = GetPlayerFromServerId(id) 
+        if otherID == myId then -- checks to see if player is in range of own messages idk why broke if it wasnt there
+            TriggerEvent('chatMessage', "", {0, 128, 255}, firstname.." "..lastname.. "| "..phone) -- Outputs message to sending client 
+        elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(myId)), GetEntityCoords(GetPlayerPed(otherID)), true) < 19.999 then -- checks for players within range
+          TriggerEvent('chatMessage', "", {0, 128, 255}, firstname.." "..lastname.. "| "..phone) -- Outputs message to players near sending client
+        end
+    end)
+end)
